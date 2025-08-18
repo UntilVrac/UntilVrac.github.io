6514e49eca1b5cf09feeaa41a72f7fac053049e2
UntilVrac <alexis.guibert.vrac@gmail.com>
2025-08-18 20:28:58 +0000

    save propre - 22:28

---
+++ SET_BRICKSTOCK_PATH.py
@@ -0,0 +1,36 @@
+from os import listdir
+
+# BRICKSTOCK_PATH = "/Users/alexis/Desktop/LEGO/BrickStock/BrickStock 2.1"
+BRICKSTOCK_PATH = "/workspaces/UntilVrac.github.io"
+
+n = 0
+
+def set_brickstock_path(current_path:str) -> None :
+    global n
+    for f in listdir(current_path) :
+        if f != "SET_BRICKSTOCK_PATH.py" :
+            path = current_path + "/" + f
+            if f.split(".")[-1] == "py" :
+                file = open(path, "r")
+                content = file.read()
+                file.close()
+                if "BRICKSTOCK_PATH = " in content :
+                    content = content.split("\n")
+                    content_bis = []
+                    for e in content :
+                        if e.startswith("BRICKSTOCK_PATH = ") :
+                            e = f'BRICKSTOCK_PATH = "{BRICKSTOCK_PATH}"'
+                        elif "BRICKSTOCK_PATH" in e :
+                            print(e)
+                        content_bis.append(e)
+                    file = open(path, "w")
+                    file.write("\n".join(content_bis))
+                    file.close()
+                    n += 1
+            elif "." not in f :
+                set_brickstock_path(path)
+
+if __name__ == "__main__" :
+    set_brickstock_path(BRICKSTOCK_PATH)
+    n = 0
+    print(f"end : {n} files affected")
